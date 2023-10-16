3a5fc143afee3817c48b11475f9b20c837d2a600
Anant Sunilkumar Patil <patilanant0010@gmail.com>
2023-10-16 12:36:08 +0530

    anurags commit

--- textutils/views.py
+++ textutils/views.py
@@ -1,65 +1,39 @@
-#i have created this file
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
+from googletrans import Translator
 
 def index(request):
-    # return HttpResponse('''<a href="https://moodle.coep.org.in/moodle/login/index.php">COEP MOODLE</a>''')
-    
-    return render(request,'index.html')
-    
-def analyse(request):
-    #get the text
-    text1 = request.POST.get('text','default')
-    # check checkbox value
-    removepunc = request.POST.get('removepunc','off')
-    capitilize = request.POST.get('capitilize','off')
-    newlineremover = request.POST.get('newlineremover','off')
-    extraspaceremover = request.POST.get('extraspaceremover','off')
-    # print(text1)
-    # print(removepunc)
-    
-    if (removepunc == 'on'):
-    
-          punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-          analysed =''
-          for char in text1:
-               if char not in punctuations:
-                  analysed = analysed + char
-          params ={'purpose':'Removed Punctuations','analysed_text':analysed,}
-          text1=analysed
-        #   return render(request,'analyse.html',params)
-    
-    if (capitilize=="on"):
-        analysed=''
-        for char in text1:
-            analysed=analysed+char.upper()
-        params ={'purpose':'Capitilse text','analysed_text':analysed,}
-        text1=analysed
-        # return render(request,'analyse.html',params)
-    
-    if (newlineremover=="on"):
-        analysed=""
-        for char in text1:
-            if char !="\n" and char !='\r':
-                analysed=analysed +char
-        params ={'purpose':'New Line Remover','analysed_text':analysed,}
-        text1=analysed
-        # return render(request,'analyse.html',params)                      
-    
-    if (extraspaceremover =="on"):
-        analysed=""
-        for index,char in enumerate(text1):
-            if not(text1[index] ==" " and text1[index +1] ==" "):
-                analysed=analysed +char
-        params ={'purpose':'extra space Remover','analysed_text':analysed,}
-        text1=analysed
-        # return render(request,'analyse.html',params)
-    
-    if(extraspaceremover !="on" and newlineremover !="on" and capitilize !="on" and removepunc !="on"):
-        return HttpResponse("Error")
-    return render(request,'analyse.html',params)
+    return render(request, 'index.html')
 
-# def spaceremover(request):
-#     return HttpResponse("space remover")
+def analyse(request):
+    if request.method == 'POST':
+        text1 = request.POST.get('text', 'default')
+        removepunc = request.POST.get('removepunc', 'off')
+        capitalize = request.POST.get('capitalize', 'off')
+        newlineremover = request.POST.get('newlineremover', 'off')
+        extraspaceremover = request.POST.get('extraspaceremover', 'off')
+        translate_to = request.POST.get('translate_to', 'off')
+
+        if not (removepunc == 'off' and capitalize == 'off' and newlineremover == 'off' and extraspaceremover == 'off' and translate_to == 'off'):
+            if removepunc == 'on':
+                punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+                text1 = ''.join(char for char in text1 if char not in punctuations)
+
+            if capitalize == 'on':
+                text1 = text1.upper()
+
+            if newlineremover == 'on':
+                text1 = text1.replace('\n', '').replace('\r', '')
+
+            if extraspaceremover == 'on':
+                text1 = ' '.join(text1.split())
+
+            if translate_to == 'on':
+                translator = Translator()
+                translated = translator.translate(text1, dest='mr')
+                text1 = translated.text
+
+            params = {'purpose': 'Text Analysis', 'analysed_text': text1}
+            return render(request, 'analyse.html', params)
+
+    return HttpResponse("Error: Please select at least one text analysis option")
